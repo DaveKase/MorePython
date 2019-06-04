4df40d8fdb11238f63f1cfe0c2c6d145c75c47d2
taaviKase <taavikase@gmail.com>
2019-06-04 16:02:50 +0300

    Table creation and query

--- src/root/nested/main.py
+++ src/root/nested/main.py
@@ -34,11 +34,6 @@ def readFile(filename):
     print(f.read())
     f.close()
 
-'''
-fileTest = open(filename, permRead)
-print(fileTest.read())
-fileTest.close
-'''
 fileTest = open(filename, permAppend)
 fileTest.write("Now there's content in the fileTest\n")
 fileTest.close()
@@ -76,3 +71,65 @@ for db in mycursor:
 '''
 
 # mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))") # Already exists
+# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") # Already done
+
+"""
+Did this already
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+name = "John"
+address = "Highway 21"
+val = (name, address)
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
+
+
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Michelle", "Blue Village")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)
+
+"""
+
+mycursor.execute("SELECT * FROM customers")
+results = mycursor.fetchall()
+
+for result in results:
+    print(result)
+
+mycursor.execute("SELECT name, address FROM customers")
+results = mycursor.fetchall()
+
+for result in results:
+    print(result)
+
