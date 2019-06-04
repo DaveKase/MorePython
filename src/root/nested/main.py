5b1d6b3637275b90ebb049021d12bf0c41f0928d
taaviKase <taavikase@gmail.com>
2019-06-04 15:42:12 +0300

    FileHandle and MySQL Connection

--- src/root/nested/main.py
+++ src/root/nested/main.py
@@ -3,6 +3,7 @@ Created on 04.06.2019
 
 @author: Taavi Kase
 '''
+import mysql.connector
 
 print("Hello World")
 x = 4
@@ -21,12 +22,57 @@ print
 print("file handling")
 print
 
+#Append to file
 filename = "fileTest.txt"
 permAppend = "a"
 permRead = "r"
-fileTest = open(filename, permAppend)
-fileTest.write("Now there's content in the fileTest")
-fileTest.close()
+permWrite = "w"
 
+# Read file
+def readFile(filename):
+    f = open(filename, permRead)
+    print(f.read())
+    f.close()
+
+'''
 fileTest = open(filename, permRead)
 print(fileTest.read())
+fileTest.close
+'''
+fileTest = open(filename, permAppend)
+fileTest.write("Now there's content in the fileTest\n")
+fileTest.close()
+readFile(filename)
+
+# Overwrite file
+fileTest = open(filename, permWrite)
+fileTest.write("New Content\n")
+fileTest.close
+readFile(filename)
+
+# Bit more appending
+fileTest = open(filename, permAppend)
+fileTest.write("Now there's content in the fileTest\n")
+fileTest.close()
+
+readFile(filename)
+
+hostName = "localhost"
+userName="root"
+pwd = "root"
+pyDb="pyTestDb"
+
+mydb = mysql.connector.connect(host=hostName, user=userName, passwd=pwd, database=pyDb)
+print(mydb)
+
+
+mycursor = mydb.cursor()
+#mycursor.execute("CREATE DATABASE pyTestDb") # Already had that db
+#mycursor.execute("SHOW DATABASES")
+
+'''
+for db in mycursor:
+    print(db)
+'''
+
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))") # Already exists
