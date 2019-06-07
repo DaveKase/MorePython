ff3492cdb1bbc9d305916ce58d6dd9b34c28de28
taaviKase <taavikase@gmail.com>
2019-06-07 12:23:55 +0300

    Removed some code

--- src/root/nested/main.py
+++ src/root/nested/main.py
@@ -4,12 +4,17 @@ Created on 04.06.2019
 @author: Taavi Kase
 '''
 import mysql.connector
+import os
 
 print("Hello World")
 x = 4
 print(x)
 x = "Sally"
 print(x)
+
+
+cwd = os.getcwd()
+print(cwd)
 
 # Python is not type sensitive
 s = "awesome"
@@ -51,113 +56,5 @@ fileTest.write("Now there's content in the fileTest\n")
 fileTest.close()
 
 readFile(filename)
-
-print()
-print("Database and table")
-print()
-
-hostName = "localhost"
-userName="root"
-pwd = "root"
-pyDb="pyTestDb"
-
-mydb = mysql.connector.connect(host=hostName, user=userName, passwd=pwd, database=pyDb)
-
-#mycursor = mydb.cursor()
-
-
-mycursor = mydb.cursor(buffered=True)
-
-#mycursor.execute("CREATE DATABASE pyTestDb") # Already had that db
-#mycursor.execute("SHOW DATABASES")
-
-'''
-for db in mycursor:
-    print(db)
-'''
-
-# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))") # Already exists
-# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") # Already done
-
-"""
-Did this already
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-name = "John"
-address = "Highway 21"
-val = (name, address)
-mycursor.execute(sql, val)
-
-mydb.commit()
-
-print(mycursor.rowcount, "record inserted.")
-
-
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
-
-mycursor.executemany(sql, val)
-
-mydb.commit()
-
-print(mycursor.rowcount, "was inserted.")
-
-
-
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("Michelle", "Blue Village")
-mycursor.execute(sql, val)
-
-mydb.commit()
-
-print("1 record inserted, ID:", mycursor.lastrowid)
-
-"""
-
-mycursor.execute("SELECT * FROM customers")
-results = mycursor.fetchall()
-
-for result in results:
-    print(result)
-
-mycursor.execute("SELECT name, address FROM customers")
-results = mycursor.fetchall()
-
-for result in results:
-    print(result)
-
-mycursor.execute("SELECT * FROM customers")
-myresult = mycursor.fetchone()
-
-print() # Printing empty line
-print(myresult)
-print()
-
-# Preventing SQL injections
-sql = "SELECT * FROM customers WHERE address = %s"
-adr = ("Yellow Garden 2", )
-
-mycursor.execute(sql, adr)
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
-
-print()
-print()
 
 
